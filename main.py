0767461d0668a272174b536f5db94b6d4e3278ed
Stanley Zhong <stanley.zhong@hp.com>
2023-02-22 16:15:47 +0800

    update first

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("AA")
+import os
+
+import discord
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'MTA3Nzg1OTg0MTY2MzU3NDA5Ng.GKFrXU.enKsS0YtI5WRLU0MqEiar_zGVOUKuaALv8kV_U'
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'hello candle':
+        await message.channel.send('hello')
+client.run(TOKEN)
